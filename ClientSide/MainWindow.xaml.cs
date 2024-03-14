caf802c53a393e9259ab2054a01e2f272f8343ab
Hemze_jx83@itstep.edu.az <Hemze_jx83@itstep.edu.az>
2024-03-14 16:26:57 +0400

    Add project files.

---
+++ ClientSide/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace ClientSide
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public UdpClient client;
+        public IPAddress remoteIP;
+        public int remotePort;
+        public IPEndPoint remoteEP;
+
+        public bool isCheck = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            remoteIP = IPAddress.Parse("127.0.0.1");
+            remotePort = 27001;
+            remoteEP = new IPEndPoint(remoteIP, remotePort);
+
+            client = new UdpClient();
+        }
+
+        private async void ShowScreenShot(object sender, RoutedEventArgs e)
+        {
+            if (!isCheck)
+            {
+                isCheck = true;
+                var buffer = new byte[ushort.MaxValue - 29];
+
+
+                await client.SendAsync(buffer, buffer.Length, remoteEP);
+
+                var list = new List<byte>();
+                var maxLen = buffer.Length;
+                var len = 0;
+
+                while (true)
+                {
+                    do
+                    {
+                        try
+                        {
+                            var result = await client.ReceiveAsync();
+
+                            buffer = result.Buffer;
+                            len = buffer.Length;
+                            list.AddRange(buffer);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+
+                    } while (len == maxLen);
+
+                    var image = await ByteToImageAsync(list.ToArray());
+                    if (image is not null)
+                        ScreenImage.Source = image;
+
+                    list.Clear();
+                }
+            }
+        } 
+
+        public async Task<BitmapImage> ByteToImageAsync(byte[] bytes)
+        {
+            var image = new BitmapImage();
+            image.BeginInit();
+
+            image.StreamSource = new MemoryStream(bytes);
+            image.CacheOption = BitmapCacheOption.OnLoad;
+
+            image.EndInit();
+            return image;
+        }
+
+
+    }
+}
---
+++ ServerSide/Program.cs
@@ -0,0 +1,58 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 27001;
+
+var listenerEP = new IPEndPoint(ip, port);
+var remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+var listener = new UdpClient(listenerEP);
+Console.WriteLine(listener.Client.LocalEndPoint + " => Listener Connected...");
+while (true)
+{
+    var result = await listener.ReceiveAsync();
+    Console.WriteLine(result.RemoteEndPoint + " => Client Connected...");
+    remoteEP = result.RemoteEndPoint;
+
+    _ = Task.Run(async () =>
+    {
+
+        while (true)
+        {
+            var screen = await TakeScreenShotAsync();
+            var imageBytes = await ImageToByteAsync(screen);
+
+            var chunks = imageBytes.Chunk(ushort.MaxValue - 29);
+
+            foreach (var chunk in chunks)
+                await listener.SendAsync(chunk, chunk.Length, remoteEP);
+        }
+    });
+}
+
+
+async Task<System.Drawing.Image> TakeScreenShotAsync()
+{
+    var width = Screen.PrimaryScreen.Bounds.Width;
+    var height = Screen.PrimaryScreen.Bounds.Height;
+    Bitmap bitmap = new Bitmap(width, height);
+
+    using Graphics? g = Graphics.FromImage(bitmap);
+    g?.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+
+    return bitmap;
+}
+
+
+async Task<byte[]> ImageToByteAsync(System.Drawing.Image image)
+{
+    using MemoryStream ms = new MemoryStream();
+    image.Save(ms, ImageFormat.Jpeg);
+
+    return ms.ToArray();
+}
